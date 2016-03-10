cba8e90a9afea362f88ad84c30549999749ba105
samurdhilbk <samurdhilbk@gmail.com>
2016-03-10 17:24:01 +0530

    First Commit

---
+++ remotecore/org/eclipse/wst/ide/remote/core/internal/provisional/Response.java
@@ -0,0 +1,10 @@
+package org.eclipse.wst.ide.remote.core.internal.provisional;
+
+public class Response {
+	
+	public static String getResponse(String command){
+		
+		return "Running "+command+" ....";
+		
+	}
+}
